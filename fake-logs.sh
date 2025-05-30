4041ccdeb64ec04b23abc27c21ccdd086647b407
sunilbittu <pavanksunil@gmail.com>
2025-05-30 22:50:10 +0530

    Updated fake-logs.sh

--- fake-logs.sh
+++ fake-logs.sh
@@ -1,18 +1,19 @@
-# .env  (replace with your values)
-NEW_RELIC_LICENSE_KEY=cebd051df3d01a74af890e4b0f96a4adFFFFNRAL
-LOG_API=https://log-api.newrelic.com/log/v1
+#!/bin/bash
 
-# fake-logs.sh
+LOG_API="https://log-api.newrelic.com/log/v1"
+NEW_RELIC_LICENSE_KEY="cebd051df3d01a74af890e4b0f96a4adFFFFNRAL"
+
 for i in {1..100}; do
-  curl -X POST $LOG_API \
-       -H "Content-Type: application/json" \
-       -H "X-License-Key: $NEW_RELIC_LICENSE_KEY" \
-       -d '[
-             {
-               "timestamp": '"$(date +%s%3N)"',
-               "message": "TEST_ERROR line '"$i"'",
-               "log.level": "error",
-               "service.name": "demo-generator"
-             }
-           ]'
+  timestamp=$(date +%s%3N)
+  curl -s -X POST "$LOG_API" \
+    -H "Content-Type: application/json" \
+    -H "X-License-Key: $NEW_RELIC_LICENSE_KEY" \
+    -d "[
+    {
+      \"timestamp\": $timestamp,
+      \"message\": \"TEST_ERROR line $i\",
+      \"log.level\": \"error\",
+      \"service.name\": \"demo-generator\"
+    }
+  ]"
 done
